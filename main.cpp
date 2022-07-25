1324696b1d71c305dcce83fbaf10738619d7028d
Andrew Knowles <87070974+AndrewJKnowles@users.noreply.github.com>
2022-07-25 16:59:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,11 @@
 *    ===============
 *
 *    Function:               Basic functionality test of the Nokia N5110 LCD display and N5110.h
-*    Circuit Schematic No.:  9     : https://github.com/ELECXJEL2645/Circuit_Schematics
+*    Circuit Schematic No.:  9
 *    Required Libraries:     N5110 : https://github.com/ELECXJEL2645/N5110
 *
 *    Authored by:            Andrew Knowles
-*    Date:                   2021
-*    Collaberators:          Dr Craig Evans
-*                            Dr Alexander Valavanis
-*                            Dr Tim Amsdon
+*    Date:                   06/2022
 *    Version:                2.0
 *    Revision Date:          06/2022 
 *    MBED Studio Version:    1.4.1
